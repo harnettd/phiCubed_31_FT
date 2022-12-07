13e1241887a82d6b372d3c39e887dddf95cbe8f1
Derek Harnett <derek.harnett@shaw.ca>
2022-12-06 17:56:04 -0800

    add spatial_integral_tplquad function

--- integrate_phiCubed_31.py
+++ integrate_phiCubed_31.py
@@ -13,7 +13,7 @@ from __future__ import print_function
 from pySecDec.integral_interface import IntegralLibrary
 import numpy as np
 from sympy import symbols, sympify, I, expand
-from scipy.integrate import trapezoid
+from scipy.integrate import trapezoid, tplquad
 
 # declare sympy symbols, i.e., variables
 value, uncertainty, eps = symbols('value uncertainty eps')
@@ -165,6 +165,57 @@ def spatial_integral(p1_space, p2_space, Delta_1, Delta_2, Delta_3):
     missing_factor = -1  # factor omitted from pySecDec generate file
     space_int_str = space_int_psd(complex_parameters=[p1p1, p2p2, p1p2, Delta_1, Delta_2, Delta_3])[2]
     return expand(missing_factor*get_value(psd_to_sympy(space_int_str)))
+
+
+def spatial_integral_tplquad(p1_space, p2_space, Delta_1, Delta_2, Delta_3):
+    """
+    Compute the spatial integral.
+
+    Parameters
+    ----------
+    p1_space : array_like, rank-0 or rank-1, complex
+        External (Euclidean) spatial momentum.
+    p2_space : array_like, rank-0 or rank-1, complex
+        External (Euclidean) spatial momentum.
+    Delta_1 : complex
+        Squared mass parameter.
+    Delta_2 : complex
+        Squared mass parameter.
+    Delta_3 : complex
+        Squared mass parameter.
+
+    Returns
+    -------
+    sympy expression
+        Spatial integral.
+    """
+    # TODO: Should I test p1_space and p2_space before the next two lines?
+    p1_space_eucl = np.array(p1_space, dtype=float)
+    p2_space_eucl = np.array(p2_space, dtype=float)
+
+    if p1_space_eucl.ndim != 1:
+        print('Error in spatial_integral: p1_space is not rank 1')
+        return None
+    if p1_space_eucl.size != 3:
+        print('Error in spatial_integral: p1_space does not have 3 elements')
+        return None
+    if p2_space_eucl.ndim != 1:
+        print('Error in spatial_integral: p2_space is not rank 1')
+        return None
+    if p2_space_eucl.size != 3:
+        print('Error in spatial_integral: p2_space does not have 3 elements')
+        return None
+
+    p1x, p1y, p1z = p1_space_eucl
+    p2x, p2y, p2z = p2_space_eucl
+    def integrand(kx, ky, kz):
+        denom_factor_1 = kx**2 + ky**2 + kz**2 + Delta_3
+        denom_factor_2 = (kx + p2x)**2 + (ky + p2y)**2 + (kz + p2z)**2 + Delta_1
+        denom_factor_3 = (kx - p1x)**2 + (ky - p1y)**2 + (kz - p1z)**2 + Delta_2
+        return 1/(8*np.pi**3 * denom_factor_1 * denom_factor_2 * denom_factor_3)
+
+    result = tplquad(integrand, -np.inf, np.inf, -np.inf, np.inf, -np.inf, np.inf)
+    return result
 
 
 def corr_zero_temp(p1, p2, m1, m2, m3, method="psd", k0_eucl_max=10,
