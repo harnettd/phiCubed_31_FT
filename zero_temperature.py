f2ce1957e97add96f93ff6a171886d4b6b581e7c
Derek Harnett <derek.harnett@shaw.ca>
2023-04-12 17:34:26 -0700

    refactor zero_tempeature.py

--- zero_temperature.py
+++ zero_temperature.py
@@ -1,14 +1,14 @@
 """Compute the zero temperature correlator.
 
 Functions:
-    correlator_use_psd(array-like, array-like, complex, complex, complex)
+    correlator_use_psd(sequence, sequence, sequence)
+        -> sympy object, sympy object
+
+    correlator_use_trapezoid(sequence, sequence, sequence, array)
         -> sympy object
 
-    correlator_use_trapezoid(array-like, array-like, complex, complex, complex, array)
-        -> sympy object
-
-    dimensionless_vertex_funxtion(sequence, sequence, sequence, float) 
-        -> sympy object
+    dimensionless_vertex_funxtion(sequence, sequence, sequence, float)
+        -> sympy object, sympy object
 """
 import numpy as np
 from scipy.integrate import trapezoid
@@ -17,8 +17,9 @@ from sympy import expand, I
 from pySecDec.integral_interface import IntegralLibrary
 
 from dot_product import dot_product
-from pysecdec_output_tools import get_value, psd_to_sympy
+from pysecdec_output_tools import get_uncertainty, get_value, psd_to_sympy
 import spatial_integral as spint
+from wick_rotation import to_euclidean
 
 REL_ERROR = 1e-6
 ABS_ERROR = 1e-9
@@ -29,74 +30,93 @@ spacetime_int_psd = IntegralLibrary('phiCubed_31/phiCubed_31_pylink.so')
 spacetime_int_psd.use_Vegas(flags=0, epsrel=REL_ERROR, epsabs=ABS_ERROR, maxeval=MAX_ITER)
 
 
-def correlator_use_psd(p1, p2, mass_1, mass_2, mass_3):
+def correlator_use_psd(p1, p2, masses):
     """Compute the zero temperature correlator using pySecDec.
 
     Parameters:
-        p1 (rank-one with four complex elements): First Minkowski external
+        p1 (four-element sequence of complex): First Minkowski external
             four-momentum
-        p2 (rank-one with four complex elements): Second Minkowski external
+        p2 (four-element sequence of complex): Second Minkowski external
             four-momentum
-        mass_1 (complex): First propagator mass
-        mass_2 (complex): Second propagator mass
-        mass_3 (complex): Third propagator mass
+        masses (three-element sequence of float): Propagator masses
 
     Returns:
-        sympy object: Zero temperature correlator as a Laurent series
+        sympy object: Zero temperature correlator value
+        sympy object: Zero temperature correlator uncertainty
     """
-    p1p1 = dot_product(p1, p1)
-    p2p2 = dot_product(p2, p2)
-    p1p2 = dot_product(p1, p2)
+    def apply_prefactor(accessor, sympy_result, prefactor):
+        """Multiply pySecDec result by the prefactor.
 
-    psd_str_result = spacetime_int_psd(
-            complex_parameters=[p1p1, p2p2, p1p2, mass_1**2, mass_2**2, mass_3**2]
-        )[2]
+        Parameters:
+            accessor (function): get_value or get_uncertainty
+            sympy_result (sympy object): pySecDec result
+            prefactor (sympy object): Additional prefactor
 
+        Returns:
+            sympy object: Zero temperature correlator value or uncertainty
+        """
+        return expand(prefactor * accessor(sympy_result))
+
+    dot_products =\
+        dot_product(p1, p1), dot_product(p2, p2), dot_product(p1, p2)
+    squared_masses = masses[0]**2, masses[1]**2, masses[2]**2
+
+    psd_str_results =\
+        spacetime_int_psd(complex_parameters=[*dot_products, *squared_masses])
+    psd_str_result = psd_str_results[2]
+
+    psd_sympy_result = psd_to_sympy(psd_str_result)
     missing_prefactor = I  # missing from the generate file
-    return expand(missing_prefactor * get_value(psd_to_sympy(psd_str_result)))
+    value = apply_prefactor(get_value, psd_sympy_result, missing_prefactor)
+    # Problems here:
+    uncertainty =\
+        apply_prefactor(get_uncertainty, psd_sympy_result, missing_prefactor)
+
+    return value, uncertainty
 
 
-def correlator_use_trapezoid(p1, p2, mass_1, mass_2, mass_3, k0_eucl_grid):
+def correlator_use_trapezoid(p1, p2, masses, k0_eucl_grid):
     """Compute the zero temperature correlator through iterated integrals.
 
     Parameters:
-        p1 (rank-one with four complex elements): First Minkowski external
+        p1 (four-element sequence of complex): First Minkowski external
             four-momentum
-        p2 (rank-one with four complex elements): Second Minkowski external
+        p2 (four-element sequence of complex): Second Minkowski external
             four-momentum
-        mass_1 (complex): First propagator mass
-        mass_2 (complex): Second propagator mass
-        mass_3 (complex): Third propagator mass
+        masses (three-element sequence of float): Propagator masses
         k0_eucl_grid (array): Grid to be used in the trapezoid rule
 
     Returns:
         sympy object: Zero temperature correlator
     """
-    p1_0_eucl = -1j * p1[0]
-    p2_0_eucl = -1j * p2[0]
+    p1_eucl = to_euclidean(p1)
+    p2_eucl = to_euclidean(p2)
+    p1_t_eucl = p1_eucl[0]
+    p2_t_eucl = p2_eucl[0]
     p1_space = p1[1:]
     p2_space = p2[1:]
 
     spatial_integral_data = []
     for k0_eucl in k0_eucl_grid:
-        delta_1 = mass_1**2 + (k0_eucl + p2_0_eucl)**2
-        delta_2 = mass_2**2 + (k0_eucl - p1_0_eucl)**2
-        delta_3 = mass_3**2 + k0_eucl**2
-        spatial_integral_data.append(spint.use_psd(p1_space, p2_space, delta_1, delta_2, delta_3))
+        delta_1 = masses[0]**2 + (k0_eucl + p2_t_eucl)**2
+        delta_2 = masses[1]**2 + (k0_eucl - p1_t_eucl)**2
+        delta_3 = masses[2]**2 + k0_eucl**2
+        deltas = delta_1, delta_2, delta_3
+        spatial_integral_data.append(spint.use_psd(p1_space, p2_space, *deltas))
 
-    return expand(-I / (2 * np.pi) * trapezoid(spatial_integral_data, k0_eucl_grid))
+    return expand(-I / (2 * np.pi) *\
+        trapezoid(spatial_integral_data, k0_eucl_grid))
 
 
 def dimensionless_correlator_use_psd(q1, q2, xis, M):
     """Compute the dimensionless zero temperature correlator using pySecDec.
 
     Parameters:
-        q1 (four-element complex sequence): First dimensionless Minkowski
+        q1 (four-element sequence of complex): First dimensionless Minkowski
             external momentum
-        q2 (four-element complex sequence): Second dimensionless Minkowski
+        q2 (four-element sequence of complex): Second dimensionless Minkowski
             external momentum
-        xis (three-element positive real sequence): Dimensionless propagator
-            masses
+        xis (three-element sequence of float): Dimensionless propagator masses
         M (real): Mass scale, i.e., the largest propagator mass
 
     One element of xis should be 1. The other two should be less than or equal
@@ -114,4 +134,8 @@ def dimensionless_correlator_use_psd(q1, q2, xis, M):
     p2 = scale_up(q2, M)
     masses = scale_up(xis, M)
 
-    return expand(correlator_use_psd(p1, p2, *masses) * M**2)
+    correlator_result = correlator_use_psd(p1, p2, masses)
+    value = expand(correlator_result[0] * M**2)
+    uncertainty = expand(correlator_result[1] * M**2)
+
+    return value, uncertainty
