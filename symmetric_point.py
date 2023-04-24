4e565e01653298d5a5f5adf297705b06dfb23458
Derek Harnett <derek.harnett@shaw.ca>
2023-04-24 14:17:36 -0700

    update docstrings in symmetric_point.py

--- symmetric_point.py
+++ symmetric_point.py
@@ -1,4 +1,7 @@
-"""Docstring
+"""Determine 4-vectors at the symmetric point for a 3-point correlator.
+
+Functions:
+    p1_p2(float) -> array, array
 """
 import numpy as np
 
@@ -6,27 +9,22 @@ def p1_p2(qq):
     """Compute 4-vectors corresponding to the symmetric point qq.
 
     Parameters
-    ----------
-    qq : real
-        Symmetric point value.
+        qq (float): Symmetric point value
 
-    Returns
-    -------
-    p1 : 4d complex array
-        4-momentum.
-    p2 : 4d complex array
-        4-momentum.
+    Returns:
+        p1 (4d array of complex): First 4-momentum
+        p2 (4d array of complex): Second 4-momentum
     """
     if qq >= 0:
-        p1x = np.sqrt(qq)*1j
-        p2t = 0.5*np.sqrt(3*qq)
+        p1x = np.sqrt(qq) * 1j
+        p2t = 0.5 * np.sqrt(3 * qq)
     elif qq < 0:
         p1x = np.sqrt(np.abs(qq))
-        p2t = 0.5*np.sqrt(3*np.abs(qq))*1j
+        p2t = 0.5 * np.sqrt(3*np.abs(qq)) * 1j
     else:
         pass  # error
     p1t = 0
-    p2x = -0.5*p1x
+    p2x = -0.5 * p1x
     p1 = np.array([p1t, p1x, 0, 0])
     p2 = np.array([p2t, p2x, 0, 0])
 
