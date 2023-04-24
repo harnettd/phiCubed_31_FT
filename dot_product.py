75a948146fafcf0ef158ad8c6347cb4af9cdc782
Derek Harnett <derek.harnett@shaw.ca>
2023-04-24 14:12:01 -0700

    update docstrings and add tests to dot_product.py

--- dot_product.py
+++ dot_product.py
@@ -1,26 +1,23 @@
-"""Docstring
+"""Calculate the dot product of two Minkowski vectors.
+
+Functions:
+    dot_product(sequence, sequence) -> complex
 """
 import numpy as np
 
 
 def dot_product(vector_1, vector_2):
-    """Compute the Minkowski dot product of v1 and v2.
+    """Compute the Minkowski dot product of vector_1 and vector_2.
 
-    v1 and v2 should be array_like objects with equal positive-integer
-    dimensions and with complex elements.
-    The first component is assumed to be the timelike one.
+    vector_1 and vector_2 should be complex sequences with equal 
+    lengths. The first components are assumed to be timelike.
 
-    Parameters
-    ----------
-    v1 : array_like, complex
-        Minkowski vector.
-    v2 : array_like, complex
-        Minkowski vector.
+    Parameters:
+        vector_1 (sequence of complex): Minkowski vector
+        vector_2 (sequence of complex): Minkowski vector
 
     Returns
-    -------
-    Complex.
-        Dot product of v1 and v2.
+        complex: Dot product of v1 and v2
     """
     v1_arr = np.array(vector_1)
     v2_arr = np.array(vector_2)
@@ -28,9 +25,15 @@ def dot_product(vector_1, vector_2):
         print('Error in dot_product: v1 and v2 are incompatible')
         return None
     if v1_arr.ndim == 0:
-        return v1_arr*v2_arr
+        return v1_arr * v2_arr
     elif v1_arr.ndim == 1:
-        return v1_arr[0]*v2_arr[0] - sum(v1_arr[1:]*v2_arr[1:])
+        return v1_arr[0] * v2_arr[0] - sum(v1_arr[1:] * v2_arr[1:])
     else:
         print('Error in dot_product: v1 and/or v2 not rank-1 objects')
         return None
+    
+
+if __name__ == '__main__':
+    print(dot_product(2, 3))
+    print(dot_product([1, 1], [3, 2]))
+    print(dot_product([1j, 1], [3j, 2]))
