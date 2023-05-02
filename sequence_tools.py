e7b173bb8b4256ce8ef77d32a5aead5409339def
Derek Harnett <derek.harnett@shaw.ca>
2023-05-02 14:34:19 -0700

    move get_col and add_lists to sequence_tools.py

--- sequence_tools.py
+++ sequence_tools.py
@@ -4,6 +4,20 @@ Functions:
     scale(sequence) -> sequence
 """
 
+
 def scale(seq, scalar):
     """Multiply each element of the sequence seq by scalar."""
     return [s * scalar for s in seq]
+
+
+def get_col(arr, col):
+    """Return the column col of array arr."""
+    return [row[col] for row in arr]
+
+
+def add_lists(seq_1, seq_2):
+    """Add two sequences element-by-element.
+
+    It is assumed that the two lists have equal length.
+    """
+    return [seq_1[index] + seq_2[index] for index in range(len(seq_1))]
